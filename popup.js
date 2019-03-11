dd8072574ce81d97e7ca62036f576dd1f17d880e
lqli <lqli@alauda.io>
2019-03-11 19:05:41 +0800

    extension init

---
+++ popup.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+
+function click(e) {
+  chrome.tabs.executeScript(null,
+      {code:"document.body.style.fontWeight='" + e.target.id + "'"});
+  window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
+});
